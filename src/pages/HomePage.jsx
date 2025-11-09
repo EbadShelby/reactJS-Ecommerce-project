91f22da27fb49f41fdf16a548a87ebdee41565ea
EbadShelby <ebadyasser123xd@gmail.com>
2025-11-09 16:43:58 +0800

    generate HTML for the cartItems

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import { formatMoney } from "../utils/money";
 import { Header } from "../components/Header";
 import "./HomePage.css";
 
@@ -41,7 +42,7 @@ export function HomePage({ cart }) {
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2)}
+                  ${formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/Header";
-import './NotFoundPage.css';
-export function NotFoundPage({cart}) {
+import "./NotFoundPage.css";
+export function NotFoundPage({ cart }) {
   return (
     <>
-      <Header cart={cart}/>
+      <title>Not Found</title>
+
+      <Header cart={cart} />
       <div className="not-found-container">
         <h1 className="not-found">404 | Not Found</h1>
       </div>
