c32cd808b4ef20e880bb9d89d4a6b125e8058870
EbadShelby <ebadyasser123xd@gmail.com>
2025-11-16 08:20:45 +0800

    make create order button functional

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -36,7 +36,7 @@ export function CheckoutPage({ cart, loadCart }) {
             cart={cart}
             loadCart={loadCart}
           />
-          <PaymentSummary paymentSummary={paymentSummary} />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
